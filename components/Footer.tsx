9ebe5a79be79a04e5e5d08395f819750db813196
sintaxis9 <skizosyntax@duck.com>
2025-04-27 16:33:26 -0400

    Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <small>SalesPredictor-PMN</small>
+    </footer>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from "react";
 import Nav from "./Nav";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 
 type Props = { children: ReactNode };
 
 export default function Layout({ children }: Props) {
   return (
-    <>
+    <div className={styles.layoutContainer}>
       <Nav />
       <main className={styles.container}>{children}</main>
-    </>
+      <Footer />
+    </div>
   );
 }
